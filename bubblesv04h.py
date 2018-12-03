36458f3e17e4c001bfff50305d12067ee78f148a
kitetsu89 <40765984+kitetsu89@users.noreply.github.com>
2018-12-03 15:24:29 +0100

    Add files via upload

---
+++ bubblesv04h.py
@@ -0,0 +1,77 @@
+# importing requests modules
+import requests
+
+# import scheduler and time modules
+import schedule
+import time as t
+
+# import modules for mail
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+print("Welcome to Bubbles v0.4_for_Heroku")
+
+print("""
+Running....
+""")
+
+# test object information
+HOST = "nu.nl"
+
+# mail information
+username = SMTP_USER
+password = SMTP_PASS
+mail_host = 'smtp.gmail.com'
+
+recipient = ''
+
+# function for requesting HTTP-statuscode
+def http_req():
+        try:
+            at = t.asctime(t.localtime((t.time())))
+            r = requests.get(f"https://{HOST}")
+            u = r.status_code
+            st = f"HTTP test succesvol for {HOST}, status_code: {u} @ {at}"
+        except:
+            st = f"Unable to Connect with {HOST} @ {at}"
+        finally:
+            print(st)
+            return st
+
+# function of mail_login
+def mail_login():
+        try:
+            s = smtplib.SMTP_SSL(mail_host)
+            print(f"Successfully connected to {mail_host}")
+            return s
+        except:
+            print(f"Not connected to {mail_host}")
+
+# function for composing mail
+def mail_compose():
+    s = mail_login()
+    msg_text = http_req()
+    msg = MIMEMultipart()
+    msg.attach(MIMEText(msg_text, 'plain'))
+    msg['From'] = username
+    msg['To'] = recipient
+    msg['Subject'] = f'Log for {HOST}'
+    try:
+        s.login(username, password)
+        s.sendmail(msg['From'], msg['To'], msg.as_string())
+        print(f"successfully sent to {recipient}")
+    except:
+        print(f"unable to sent to {recipient}")
+    finally:
+        s.quit()
+
+# Scheduler
+# use function reference!!!!
+schedule.every().day.at("08:30").do(mail_compose)
+
+# Runner
+while True:
+# current time, EPOCH -> Struct -> ASCII
+    schedule.run_pending()
+    t.sleep(1)
