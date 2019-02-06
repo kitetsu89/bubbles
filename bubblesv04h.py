738b1a85ed04ca319f8031c25f8d09542b266ea1
Belhadji <kamal.belhadji@kpn.com>
2019-02-06 16:46:32 +0100

    Added a second host

--- bubblesv04h.py
+++ bubblesv04h.py
@@ -18,26 +18,31 @@ Running....
 """)
 
 # test object information
-HOST = os.environ['TEST_OBJ']
+HOST1 = os.environ['TEST_OBJ_ACC']
+HOST2 = os.environ['TEST_OBJ_PRD']
 
 # mail information
 username = os.environ['SMTP_USER']
 password = os.environ['SMTP_PASS']
 mail_host = os.environ['SMTP_SERV']
-#git testje
-# nou dit is voor de tweede keer
 recipient = os.environ['SMTP_RECP']
+
 
 # function for requesting HTTP-statuscode
 def http_req():
         try:
             at = t.asctime(t.localtime((t.time())))
-            r = requests.get(f"https://{HOST}")
-            u = r.status_code
-            st = f"HTTP test succesvol for {HOST}, status_code: {u} @ {at}"
+            r1 = requests.get(f"https://{HOST1}")
+            r2 = requests.get(f"https://{HOST2}")
+            u1 = r1.status_code
+            u2 = r2.status_code
+            st1 = f"HTTP test successful for {HOST1}, status_code: {u1} @ {at}"
+            st2 = f"HTTP test successful for {HOST2}, status_code: {u2} @ {at}"
         except:
-            st = f"Unable to Connect with {HOST} @ {at}"
+            st1 = f"Unable to Connect with {HOST1} @ {at}"
+            st2 = f"Unable to Connect with {HOST2} @ {at}"
         finally:
+            st = f"{st1}\n{st2}"
             print(st)
             return st
 
@@ -54,26 +59,33 @@ def mail_login():
 def mail_compose():
     s = mail_login()
     msg_text = http_req()
+    print(type(msg_text))
     msg = MIMEMultipart()
     msg.attach(MIMEText(msg_text, 'plain'))
     msg['From'] = username
     msg['To'] = recipient
-    msg['Subject'] = f'Log for {HOST}'
+    msg['Subject'] = "Log for RCS-proxy"
     try:
         s.login(username, password)
+        print("successfully logged in")
+    except:
+        print("unable to login")
+    try:
         s.sendmail(msg['From'], msg['To'], msg.as_string())
         print(f"successfully sent to {recipient}")
     except:
-        print(f"unable to sent to {recipient}")
+        print(f"unable to send to {recipient}")
     finally:
         s.quit()
+
+#mail_compose()
 
 # Scheduler
 # use function reference!!!!
 schedule.every().day.at("08:30").do(mail_compose)
 
 # Runner
-while True:
+#while True:
 # current time, EPOCH -> Struct -> ASCII
     schedule.run_pending()
     t.sleep(1)
